149c243e73ed81542797a3dc0f5cca8e5f1aa3b0
Burmer, Michal <Michal.Burmer@sabre.com>
2021-07-23 14:47:10 +0200

    Add Comments to endpoints

--- src/main/java/com/hopper/restapi/model/Post.java
+++ src/main/java/com/hopper/restapi/model/Post.java
@@ -5,24 +5,28 @@ import lombok.Setter;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
 @Setter
 public class Post {
 
+    @Id
     private Long id;
     private String title;
     private String content;
     private LocalDateTime created;
 
+    @OneToMany
+    @JoinColumn(name = "post_id") //DB column name, if in Comments.java would be field postId then should be postId here
+    private List<Comment> comments;
+
     public void setId(Long id) {
         this.id = id;
     }
 
-    @Id
-    public Long getId() {
-        return id;
-    }
 }
